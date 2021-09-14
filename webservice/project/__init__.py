a7dfecfc2f0aa397e5b36e81bb8068c7c1819a3b
Jivelien <julien.Delannoy@hotmail.com>
2021-09-14 22:47:46 +0200

    initial poc

---
+++ webservice/project/__init__.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+from flask import render_template
+from flask_wtf import FlaskForm
+from datetime import date
+from wtforms.fields.html5 import DateField
+from wtforms.fields.html5 import TimeField
+
+app = Flask(__name__, template_folder="/home/app/web/project")
+app.config.from_object("project.config.Config")
+db = SQLAlchemy(app)
+
+
+class Event(db.Model):
+    __tablename__ = "event"
+
+    id = db.Column(db.Integer, primary_key=True)
+    eventtime = db.Column(db.DateTime, unique=True, nullable=False)
+    label = db.Column(db.String(26), default=True, nullable=False)
+
+    def __init__(self, eventtime):
+        self.label = "cigarette"
+        self.eventtime = eventtime
+
+class TestForm(FlaskForm):
+    eventdate = DateField('Date:')
+    eventtime = TimeField('time:')
+
+@app.route("/")
+def hello_world():
+    forms = TestForm()
+    if forms.validate_on_submit():
+        return 'From Date is : {} To Date is : {}'.format(forms.startdate.data, forms.todate.data)
+    import os
+    print(f"you are here : {os.getcwd()}")
+    return render_template('test.html',form=forms)
+
+
+@app.route("/add_event", methods=['POST', 'GET'])
+def add_event():
+    if request.method == 'POST':
+        eventdate = request.form.get("eventdate")
+        eventtime = request.form.get("eventtime")
+        eventdatetime = eventdate+'T'+eventtime
+    else:
+        eventdatetime = request.args.get('t')
+
+    event = Event(eventdatetime)
+    db.session.add(event)
+    db.session.commit()
+    return jsonify(added=event.eventtime)
