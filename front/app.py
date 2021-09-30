ed67ca39451e5b6e75477341fcad5dc454b93b54
Jivelien <julien.delannoy@hotmail.com>
2021-09-30 20:57:50 +0200

    Feat/separate ws and front (#1)
    * create folder for front
    
    * front can call ws
    
    * front and ws are now separeted
    
    * redirect after post
    
    * simplify add_event fct
    
    Co-authored-by: Julien <jivelien@gmail.com>

---
+++ front/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request, redirect
+from flask import render_template
+from flask_wtf import FlaskForm
+from flask import render_template
+from flask_wtf import FlaskForm
+from datetime import date
+from wtforms.fields.html5 import DateField, TimeField, SearchField
+from wtforms.fields.html5 import TimeField
+import requests
+
+app = Flask(__name__)
+app.config.from_object("config.Config")
+
+def get_last_cigarette():
+    last_cigarette=requests.get("http://webservice:5000/get_last_cigarette").json()
+    return last_cigarette
+
+class TestForm(FlaskForm):
+    password = SearchField('Password:')
+    eventdate = DateField('Date:')
+    eventtime = TimeField('time:')
+
+
+@app.route("/")
+def main():
+    forms = TestForm()
+    last_cigarette = get_last_cigarette()
+
+    return render_template('test.html',form=forms, last_cigarette=last_cigarette)
+
+@app.route("/add_event", methods=['POST'])
+def add_event():
+    password = request.form.get("password") 
+    eventdate = request.form.get("eventdate")
+    eventtime = request.form.get("eventtime")
+    eventdatetime = eventdate+'T'+eventtime
+
+    event = {'password' : password, 'eventdatetime' : eventdatetime}
+
+    add_cigarette = requests.post("http://webservice:5000/add_event", data=event)
+
+    return redirect('/')    
+
+
+
+if __name__ == "__main__":
+    app()
--- webservice/project/__init__.py
+++ webservice/project/__init__.py
@@ -2,11 +2,6 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
-from flask import render_template
-from flask_wtf import FlaskForm
-from datetime import date
-from wtforms.fields.html5 import DateField, TimeField, SearchField
-from wtforms.fields.html5 import TimeField
 
 app = Flask(__name__, template_folder="/home/app/web/project")
 app.config.from_object("project.config.Config")
@@ -24,34 +19,20 @@ class Event(db.Model):
         self.label = "cigarette"
         self.eventtime = eventtime
 
-class TestForm(FlaskForm):
-    password = SearchField('Password:')
-    eventdate = DateField('Date:')
-    eventtime = TimeField('time:')
+@app.route("/get_last_cigarette", methods=['GET'])
+def last_cigarette():
+    return jsonify(event=Event.query.order_by(Event.eventtime.desc()).first().eventtime)
+    
 
-@app.route("/")
-def hello_world():
-    forms = TestForm()
-    last_cigarette = Event.query.order_by(Event.eventtime.desc()).first().eventtime
-    if forms.validate_on_submit():
-        return 'From Date is : {} To Date is : {}'.format(forms.startdate.data, forms.todate.data)
-    import os
-    print(f"you are here : {os.getcwd()}")
-    return render_template('test.html',form=forms, last_cigarette=last_cigarette)
-
-
-@app.route("/add_event", methods=['POST', 'GET'])
+@app.route("/add_event", methods=['POST'])
 def add_event():
-    if request.method == 'POST':
-        if request.form.get("password") != os.getenv('TMP_PASS'):
-            return "Nope, motherfucker"
-        eventdate = request.form.get("eventdate")
-        eventtime = request.form.get("eventtime")
-        eventdatetime = eventdate+'T'+eventtime
-    else:
-        eventdatetime = request.args.get('t')
+    if request.form.get("password") != os.getenv('TMP_PASS'):
+        return "Nope, motherfucker"
+
+    eventdatetime = request.form.get("eventdatetime")
 
     event = Event(eventdatetime)
     db.session.add(event)
     db.session.commit()
-    return jsonify(added=event.eventtime)
+    
+    return jsonify(event=event.eventtime)
