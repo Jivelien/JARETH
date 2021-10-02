56e943634da2810295b61406278f10c796ba37d8
Julien <jivelien@gmail.com>
2021-10-02 18:29:31 +0200

    add duration count and advice

--- front/app.py
+++ front/app.py
@@ -4,26 +4,80 @@ from flask_wtf import FlaskForm
 from flask import render_template
 from flask_wtf import FlaskForm
 from datetime import datetime
+from dateutil.parser import parse
 import requests
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
 
+__SECONDS_PER_WEEK__ = 7*60*60*24
+__SECONDS_PER_DAY__ = 60*60*24
+__SECONDS_PER_HOUR__ = 60*60
+__SECONDS_PER_MINUTE__ = 60
+
 def get_last_cigarette():
     last_cigarette=requests.get("http://webservice:5000/get_last_cigarette").json()
     return last_cigarette
 
+def duration_to_string(duration):
+    duration = int(duration)
+
+    if duration // __SECONDS_PER_MINUTE__  == 0:
+        return "just now"
+
+    absolute_duration = abs(duration)
+
+    days = absolute_duration // __SECONDS_PER_DAY__
+    seconds_left = absolute_duration - days * __SECONDS_PER_DAY__
+
+    hours = seconds_left //60//60
+    seconds_left = seconds_left - hours * __SECONDS_PER_HOUR__
+
+    minuts = seconds_left //60
+    seconds = seconds_left - minuts * __SECONDS_PER_MINUTE__
+
+    result_part = []
+    if days > 1:
+        result_part.append(f"{days} days")
+    elif days > 0:
+        result_part.append(f"{days} day")
+    if hours > 1:
+        result_part.append(f"{hours} hours")
+    elif hours > 0:
+        result_part.append(f"{hours} hour")
+    if minuts > 1:
+        result_part.append(f"{minuts} minuts")
+    elif minuts > 0:
+        result_part.append(f"{minuts} minut")
+
+    if len(result_part) > 1:
+        result_part.insert(-1, "and")
+
+    if duration > 0:
+        result_part.append("ago")
+    else:
+        result_part.insert(0,"in")
+
+    return " ".join(result_part)
+
+def get_advice(duration):
+    if duration <= 1.5 * __SECONDS_PER_HOUR__:
+        return "You should wait..."
+
 @app.route("/")
 def main():
-    last_cigarette = get_last_cigarette().get("event")
-    if last_cigarette :
-        last_cigarette_timestamp = datetime.strptime(last_cigarette, "%a, %d %b %Y %X GMT")
+    last_cigarette_timestamp = get_last_cigarette().get("event")
+    if last_cigarette_timestamp != '':
+        delta = (datetime.now() - parse(last_cigarette_timestamp)).total_seconds()
+        last_cigarette = duration_to_string(delta)
+        advice = get_advice(delta)
     else:
-        last_cigarette_timestamp = ''
-        last_cig_from_now = ''
+        last_cigarette = ''
+        advice = ''
 
     return render_template('test.html',
-                            last_cigarette=last_cigarette_timestamp)
+                            last_cigarette=last_cigarette,
+                            advice= advice)
 
 @app.route("/add_event", methods=['POST'])
 def add_event():
