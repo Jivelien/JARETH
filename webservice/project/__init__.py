8dce9968bf564d1e7f5176550fae97021520a39c
Julien <jivelien@gmail.com>
2021-10-19 11:58:57 +0200

    add get_all

--- webservice/project/__init__.py
+++ webservice/project/__init__.py
@@ -20,6 +20,13 @@ class Event(db.Model):
         self.label = "cigarette"
         self.eventtime = eventtime
 
+    @property
+    def serialize(self):
+       return {
+           'id'   : self.id,
+           'event': self.eventtime
+       }
+
 @app.route("/get_last_cigarette", methods=['GET'])
 def last_cigarette():
     # datetime.strptime("Thu, 30 Sep 2021 20:53:00 GMT", "%a, %d %b %Y %X GMT")
@@ -28,6 +35,12 @@ def last_cigarette():
         return jsonify(id= last.id, event=str(last.eventtime))
     else:
         return jsonify(id= '', event='')
+
+@app.route("/get_cigarettes", methods=['GET'])
+def get_cigarettes():
+    # datetime.strptime("Thu, 30 Sep 2021 20:53:00 GMT", "%a, %d %b %Y %X GMT")
+    cigarettes = Event.query.order_by(Event.eventtime.desc()).all()
+    return jsonify([cigarette.serialize for cigarette in cigarettes])
 
 
 @app.route("/add_event", methods=['POST'])
