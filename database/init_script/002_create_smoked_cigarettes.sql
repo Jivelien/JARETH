273bd9e593dd82a8032396c5ed5f6a4827215400
Julien <jivelien@gmail.com>
2021-11-08 00:55:50 +0100

    Code improvement for transaction and close

--- database/init_script/002_create_smoked_cigarettes.sql
+++ database/init_script/002_create_smoked_cigarettes.sql
@@ -1,9 +1,10 @@
 CREATE TABLE smoked_cigarettes (
     cigarette_id     INT         GENERATED ALWAYS AS IDENTITY,
-    public_user_id   VARCHAR(36)         NOT NULL,
-    event_time       TIMESTAMP   UNIQUE          NOT NULL,
+    public_user_id   VARCHAR(36) NOT NULL,
+    event_time       TIMESTAMP   NOT NULL,
     PRIMARY KEY(cigarette_id),
-     CONSTRAINT fk_user_cigarette
+    UNIQUE (public_user_id, event_time)
+    CONSTRAINT fk_user_cigarette
       FOREIGN KEY(public_user_id) 
 	  REFERENCES users(public_id)
 );
--- webservice/main.py
+++ webservice/main.py
@@ -4,6 +4,7 @@ import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import datetime
+from time import sleep
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
@@ -24,57 +25,66 @@ def token_requiered(f):
 
     return wrapper
 
+
 @app.route("/users", methods=['GET'],endpoint='get_all_users')
 @token_requiered
 def get_all_users(current_user):
     engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    conn = engine.connect()
-    results = conn.execute("SELECT username, public_id, mail, password FROM users").fetchall()
+    with engine.connect() as conn: 
+        results = conn.execute("SELECT username, public_id, mail, password FROM users")
 
     users = []
     for user in results:
         users.append({
-            'is_current_user' : current_user == user[1],
-            'username': user[0],
-            'public_id': user[1],
-            'mail': user[2],
-            #'password': user[3]
+            'is_current_user' : current_user == user['username'],
+            'username': user['username'],
+            'public_id': user['public_id'],
+            'mail': user['mail'],
+            #'password': user['password']
         })
 
-    conn.close()
-    return jsonify(users)
+    return Response(response = jsonify(users)
+                    , status = 200
+                    , mimetype = 'application/json')
 
 
 @app.route("/user", methods=['POST'],endpoint='create_user')
 def create_user():
     data = request.get_json()
 
-    if not data.get('username') or not data.get('mail') or not data.get('password'):
-        return Response(status=400)
+    if not data or not data.get('username') or not data.get('mail') or not data.get('password'):
+        return Response(response="Missing informations in payload"
+                        , status=400)
 
+    engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 
-        #TODO check unicity of username and mail
+    with engine.connect() as conn: 
+        result = conn.execute(f"SELECT 1 FROM users WHERE mail='{data.get('mail')}'").fetchone()
+
+    if result:
+        return Response(response="Mail address already used"
+                        , status=400)
 
     data['public_id'] = str(uuid.uuid4())
     hashed_password = generate_password_hash(data['password'], method='sha256')
 
-    engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    conn = engine.connect()
-    conn.execute(f"INSERT INTO users(public_id, username, mail, password) VALUES('{data['public_id']}',  '{data['username']}', '{data['mail']}',  '{hashed_password}')")
-    conn.close()
+    with engine.begin() as conn:
+        conn.execute(f"INSERT INTO users(public_id, username, mail, password) VALUES('{data['public_id']}',  '{data['username']}', '{data['mail']}',  '{hashed_password}')")
 
-    return  jsonify(public_id = data['public_id'],
-                    username = data['username'],
-                    mail = data['mail'],
-                    password = hashed_password)
+    return  Response(response = jsonify(public_id = data['public_id'],
+                                    username = data['username'],
+                                    mail = data['mail'],
+                                    password = hashed_password)
+                     , status = 201
+                     , mimetype = 'application/json')
 
 
 @app.route("/user/<public_id>", methods=['GET'],endpoint='get_user')
 @token_requiered
 def get_user(current_user, public_id):
     engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    conn = engine.connect()
-    result = conn.execute(f"SELECT username, public_id, mail, password FROM users WHERE public_id='{public_id}'").fetchone()
+    with engine.connect() as conn: 
+        result = conn.execute(f"SELECT username, public_id, mail, password FROM users WHERE public_id='{public_id}'").fetchone()
 
     if result:
         user = {
@@ -83,58 +93,64 @@ def get_user(current_user, public_id):
             'mail': result[2],
             'password': result[3]
         }
-        conn.close()
-        return jsonify(user)
+        return Response(response = jsonify(user)
+                        , status = 200
+                        , mimetype = 'application/json')
     else:
-        conn.close()
-        return Response(status=404)
+        return Response(response = 'Unable to find user'
+                        , status = 404)
 
 
 @app.route("/user/<public_id>", methods=['PUT'],endpoint='update_user')
 @token_requiered
 def update_user(current_user, public_id):
     if not current_user == public_id:
-        return Response(status=401)
+        return Response(message = "Permission denied to update this user"
+                        , status = 403)
 
-    data = request.get_json()
     engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    conn = engine.connect()
-    result = conn.execute(f"SELECT username, public_id, mail, password FROM users WHERE public_id='{public_id}'").fetchone()
+
+    with engine.connect() as conn: 
+        result = conn.execute(f"SELECT 1 FROM users WHERE public_id='{public_id}'").fetchone()
 
     if result:
-        if data.get('username') and data.get('mail') and data.get('password'): #FIXME
+        data = request.get_json()
+        if data and data.get('username') and data.get('mail') and data.get('password'):
             hashed_password = generate_password_hash(data['password'], method='sha256')
-            conn.execute(f"UPDATE users SET username='{data['username']}', mail='{data['mail']}', password='{hashed_password}' WHERE public_id='{public_id}'")
-            conn.close()
-            return jsonify(username=data['username'],
-                           public_id=public_id,
-                           mail=data['mail'],
-                           password=hashed_password)
+            with engine.begin() as conn: 
+                conn.execute(f"UPDATE users SET username='{data['username']}', mail='{data['mail']}', password='{hashed_password}' WHERE public_id='{public_id}'")
+
+            return Reponse(response = jsonify(username=data['username'],
+                                              public_id=public_id,
+                                              mail=data['mail'],
+                                              password=hashed_password)
+                            , status = 200)
         else:
-            conn.close()
-            return Response(status=400)
+            return Response(response="Missing informations in payload"
+                            , status=400)
     else:
-        conn.close()
-        return Response(status=404)
+        return Response(response="No user to update"
+                        , status=404)
 
 
-@app.route("/user/<public_id>", methods=['DELETE'],endpoint='delete_user')
+@app.route("/user/<public_id>", methods=['DELETE'], endpoint='delete_user')
 @token_requiered
 def delete_user(current_user, public_id):
     if not current_user == public_id:
-        return Response(status=401)
+        return Response(response="Permission denied to delete this user"
+                        , status=401)
 
     engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    conn = engine.connect()
-    result = conn.execute(f"SELECT 1 FROM users WHERE public_id='{public_id}'").fetchone()
+    with engine.connect() as conn: 
+        result = conn.execute(f"SELECT 1 FROM users WHERE public_id='{public_id}'").fetchone()
 
-    if result:
-        conn.execute(f"DELETE FROM users WHERE public_id='{public_id}'")
-        conn.close()
-        return Response(status=200)
-    else:
-        conn.close()
+    if not result:
         return Response(status=404)
+
+    with engine.begin() as conn: 
+        conn.execute(f"DELETE FROM users WHERE public_id='{public_id}'")
+    return Response(status=200)
+
     
 @app.route("/login", methods=['POST'], endpoint='login')
 def login():
@@ -143,94 +159,78 @@ def login():
         return Response(status=400)
 
     engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    conn = engine.connect()
-    result = conn.execute(f"SELECT username, public_id, mail, password FROM users WHERE mail='{data['mail']}'").fetchone()
+    
+    with engine.connect() as conn: 
+        result = conn.execute(f"SELECT username, public_id, mail, password FROM users WHERE mail='{data['mail']}'").fetchone()
 
-    if result:
-        if check_password_hash(result[3], data['password']):
-            payload = {
-                'public_id': result[1],
-                'username': result[0],
-                #'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
-            }
-            token = jwt.encode(payload, app.config['SECRET_KEY'])
-            conn.close()
-            return jsonify(public_id=result[1],
-                           token=token
-                           )
-        else:
-            conn.close()
-            return Response(status=401)
-    else:
-        conn.close()
+    if not result:
         return Response(status=404)
+
+    if check_password_hash(result[3], data['password']):
+        payload = {
+            'public_id': result[1],
+            'username': result[0],
+            #'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+        }
+        token = jwt.encode(payload, app.config['SECRET_KEY'])
+        return jsonify(public_id=result[1],
+                       token=token
+                        )
+    else:
+        sleep(seconds = 5)
+        return Response(status=401)
 
 @app.route("/cigarette", methods=['POST'], endpoint='create_cigarette')
 @token_requiered
 def create_cigarette(current_user):
     data = request.get_json()
-    if not data.get('event_time'):
+    if not data and not data.get('event_time'):
+        return Response(status=400)
+    
+    try:
+        event_time = datetime.datetime.strptime(data.get('event_time'), "%Y-%m-%d %H:%M:%S")
+    except ValueError:
         return Response(status=400)
 
-    #FIXME check if unique
-    #FIXME convert event_time to datetime
-
     engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    conn = engine.connect()
-    conn.execute(f"INSERT INTO smoked_cigarettes(public_user_id, event_time) VALUES('{current_user}', '{data['event_time']}')")
-    conn.close()
+    with engine.connect() as conn: 
+        result = conn.execute(f"SELECT 1 FROM users WHERE public_id='{current_user}' and event_time='{event_time}").fetchone()
+    if result:
+        return Response(status=400)
+        
+    with engine.begin() as conn: 
+        conn.execute(f"INSERT INTO smoked_cigarettes(public_user_id, event_time) VALUES('{current_user}', '{event_time}')")
 
-    return Response(status=200)
+    return Response(status=201)
 
 
 @app.route("/cigarettes", methods=['GET'], endpoint='get_cigarettes')
 @token_requiered
 def get_cigarettes(current_user):
     engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    conn = engine.connect()
-    results = conn.execute(f"SELECT cigarette_id, event_time FROM smoked_cigarettes WHERE public_user_id='{current_user}'").fetchall()
+    with engine.connect() as conn: 
+        results = conn.execute(f"SELECT cigarette_id, event_time FROM smoked_cigarettes WHERE public_user_id='{current_user}'")
 
     cigarettes = []
     for cigarette in results:
         cigarettes.append({
-            'id' : cigarette[0],
-            'event_time': cigarette[1].strftime('%Y-%m-%d %H:%M:%S') 
+            'id' : cigarette["cigarette_id"],
+            'event_time': cigarette["event_time"].strftime('%Y-%m-%d %H:%M:%S') 
         })
-
-    conn.close()
-    return jsonify(cigarettes)
+    return Response(response=jsonify(cigarettes)
+                    , status=200)
 
 @app.route("/cigarette/<cigarette_id>", methods=['DELETE'], endpoint='delete_cigarette')
 @token_requiered
 def delete_cigarette(current_user, cigarette_id):
     engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    conn = engine.connect()
-    result = conn.execute(f"SELECT 1 FROM smoked_cigarettes WHERE cigarette_id='{cigarette_id}' and public_user_id='{current_user}'").fetchone()
 
-    if result:
+    with engine.connect() as conn: 
+        result = conn.execute(f"SELECT 1 FROM smoked_cigarettes WHERE cigarette_id='{cigarette_id}' and public_user_id='{current_user}'").fetchone()
+
+    if not result:
+        return Response(status=404)
+
+    with engine.begin() as conn: 
         conn.execute(f"DELETE FROM smoked_cigarettes WHERE cigarette_id='{cigarette_id}' and public_user_id='{current_user}'")
-        conn.close()
-        return Response(status=200)
-    else:
-        conn.close()
-        return Response(status=404)
-
-
-@app.route("/cigarette/<cigarette_id>", methods=['PUT'], endpoint='update_cigarette')
-@token_requiered
-def update_cigarette(current_user, cigarette_id):
-    data = request.get_json()
-    if not data.get('event_time'):
-        return Response(status=400)
-
-    engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    conn = engine.connect()
-    result = conn.execute(f"SELECT 1 FROM smoked_cigarettes WHERE cigarette_id='{cigarette_id}' and public_user_id='{current_user}'").fetchone()
-
-    if result:
-        conn.execute(f"UPDATE smoked_cigarettes SET event_time='{data['event_time']}' WHERE cigarette_id='{cigarette_id}' and public_user_id='{current_user}'")
-        conn.close()
-        return Response(status=200)
-    else:
-        conn.close()
-        return Response(status=404)
+    return Response(status=200)
