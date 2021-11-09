470ea3b0a7be35b95ac6213faad3a9182cdb1497
Julien <jivelien@gmail.com>
2021-11-09 21:47:49 +0100

    Correct typo in tabl creation

--- database/init_script/002_create_smoked_cigarettes.sql
+++ database/init_script/002_create_smoked_cigarettes.sql
@@ -3,7 +3,7 @@ CREATE TABLE smoked_cigarettes (
     public_user_id   VARCHAR(36) NOT NULL,
     event_time       TIMESTAMP   NOT NULL,
     PRIMARY KEY(cigarette_id),
-    UNIQUE (public_user_id, event_time)
+    UNIQUE (public_user_id, event_time),
     CONSTRAINT fk_user_cigarette
       FOREIGN KEY(public_user_id) 
 	  REFERENCES users(public_id)
