fe226cd4652e414f2457b8d1d10f6f0d16919111
Julien <jivelien@gmail.com>
2021-12-08 00:37:38 +0100

    user model and test

--- webservice/models/user.py
+++ webservice/models/user.py
@@ -8,56 +8,44 @@ class User:
                  user_id: int = None, public_id: str = None,
                  username: str = None, mail: str = None,
                  password: str = None, hashed_password: str = None) -> None:
-        self.user_id = None
-        self.public_id = None
-        self.username = None
-        self.mail = None
-        self.password = None
-        self.hashed_password = None
-
-        self.set_public_id(public_id=public_id)
-        self.set_username(username=username)
-        self.set_mail(mail=mail)
-        self.set_password(password=password)
-
-    def set_user_id(self, user_id: int) -> None:
         self.user_id = user_id
-
-    def set_public_id(self, public_id: str) -> None:
         self.public_id = public_id
+        self.username = username
+        self.mail = mail
+        self.password = password
+        self.hashed_password = hashed_password
 
-    def set_username(self, username: str) -> None:
-        if len(username) < 3 or len(username) > 20:
+        self._control_username_format() if self.username else None
+        self._control_mail_format() if self.mail else None
+        self._control_password_format() if self.password else None
+
+    def _control_username_format(self) -> None:
+        if len(self.username) < 3 or len(self.username) > 20:
             raise ValueError('Username must be between 3 and 20 characters')
-        if not bool(re.match('^[a-zA-Z0-9]*$', username)):
+        if not bool(re.match('^[a-zA-Z0-9]*$', self.username)):
             raise ValueError('Username must contain only letters and numbers')
-        if username[0].isdigit():
+        if self.username[0].isdigit():
             raise ValueError('Username must not start with a number')
 
-        self.username = username
+    def _control_mail_format(self) -> None:
+        if not bool(re.match('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', self.mail)):
+            raise ValueError('Mail must be a valid email address')
 
-    def set_mail(self, mail: str) -> None:
-        # if not bool(re.match('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', mail)):
-        #     raise ValueError('Mail must be a valid email address')
-        self.mail = mail
-
-    def set_password(self, password: str) -> None:
-        # if len(password) < 6 or len(password) > 20:
-        #     raise ValueError('Password must be between 6 and 20 characters')
-        # if not any(char.isdigit() for char in password):
-        #     raise ValueError('Password must contain at least one number')
-        # if not any(char.isupper() for char in password):
-        #     raise ValueError('Password must contain at least one uppercase letter')
-        # if not any(char.islower() for char in password):
-        #     raise ValueError('Password must contain at least one lowercase letter')
-        # if not any(char in User.SPECIAL_CHAR for char in password):
-        #     raise ValueError('Password must contain at least one special character')
-        self.password = password
+    def _control_password_format(self) -> None:
+        if len(self.password) < 6 or len(self.password) > 20:
+            raise ValueError('Password must be between 6 and 20 characters')
+        if not any(char.isdigit() for char in self.password):
+            raise ValueError('Password must contain at least one number')
+        if not any(char.isupper() for char in self.password):
+            raise ValueError('Password must contain at least one uppercase letter')
+        if not any(char.islower() for char in self.password):
+            raise ValueError('Password must contain at least one lowercase letter')
+        if not any(char in User.SPECIAL_CHAR for char in self.password):
+            raise ValueError('Password must contain at least one special character')
 
     def to_json(self) -> dict:
         return {
             'public_id': self.public_id,
             'username': self.username,
-            'mail': self.mail,
-            'password': self.password
+            'mail': self.mail
         }
--- webservice/tests/test__user.py
+++ webservice/tests/test__user.py
@@ -3,6 +3,10 @@ from models.user import User
 
 
 class TestUsername(unittest.TestCase):
+    def test__username_can_be_created(self):
+        username = "Artem"
+        user = User(username=username)
+        self.assertEqual(user.username, username)
 
     def test__username_should_be_at_least_3chars_long(self):
         with self.assertRaises(ValueError):
@@ -10,7 +14,7 @@ class TestUsername(unittest.TestCase):
 
     def test__username_should_be_at_most_20chars_long(self):
         with self.assertRaises(ValueError):
-            User(username="a"*21)
+            User(username="a" * 21)
 
     def test__username_should_contains_normal_chars(self):
         with self.assertRaises(ValueError):
@@ -20,10 +24,58 @@ class TestUsername(unittest.TestCase):
         with self.assertRaises(ValueError):
             User(username="1Artem")
 
-    def test__username_can_be_created(self):
-        username = "Artem"
-        user = User(username=username)
-        self.assertEqual(user.username, username)
+
+class TestMail(unittest.TestCase):
+
+    def test__mail_can_be_created(self):
+        mail = "artem@gmail.com"
+        user = User(mail=mail)
+        self.assertEqual(user.mail, mail)
+
+    def test__mail_should_look_like_a_mail(self):
+        with self.assertRaises(ValueError):
+            User(mail="artem")
+        with self.assertRaises(ValueError):
+            User(mail="artem@gmail")
+        with self.assertRaises(ValueError):
+            User(mail="gmail.com.fr")
+
+
+class TestPassword(unittest.TestCase):
+    PASSWORD_EXEMPLE = "@rt3mTheBest!"
+
+    def test__password_can_be_created(self):
+        user = User(password=TestPassword.PASSWORD_EXEMPLE)
+        self.assertEqual(user.password, TestPassword.PASSWORD_EXEMPLE)
+
+    def test__password_should_be_at_least_6chars_long(self):
+        with self.assertRaises(ValueError):
+            User(password=TestPassword.PASSWORD_EXEMPLE[:5])
+
+    def test__password_should_be_at_most_20chars_long(self):
+        with self.assertRaises(ValueError):
+            User(password=TestPassword.PASSWORD_EXEMPLE * 22)
+
+    def test__password_should_contains_a_digit(self):
+        password = "".join(["a" if char.isdigit() else char for char in TestPassword.PASSWORD_EXEMPLE])
+        with self.assertRaises(ValueError):
+            User(password=password)
+
+    def test__password_should_contains_a_upper_case_letter(self):
+        password = TestPassword.PASSWORD_EXEMPLE.lower()
+        with self.assertRaises(ValueError):
+            User(password=password)
+
+    def test__password_should_contains_a_lower_case_letter(self):
+        password = TestPassword.PASSWORD_EXEMPLE.upper()
+        with self.assertRaises(ValueError):
+            User(password=password)
+
+    def test__password_should_contains_a_special_char(self):
+        password = "".join(["a" if not char.isalpha() and not char.isdigit() else char for char in TestPassword.PASSWORD_EXEMPLE])
+        with self.assertRaises(ValueError):
+            User(password=password)
+
 
 if __name__ == '__main__':
     unittest.main()
